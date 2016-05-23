848af88b2da2e97e24f05721df791ee3c7eb0a59
flhuoshan <475296958@qq.com>
2016-05-23 21:36:35 +0800

    Enum类

--- java/lang/Enum.java
+++ java/lang/Enum.java
@@ -32,19 +32,17 @@ import java.io.ObjectInputStream;
 import java.io.ObjectStreamException;
 
 /**
- * This is the common base class of all Java language enumeration types.
+ * 该类是所有Java语言枚举类型的共同基类。
  *
- * More information about enums, including descriptions of the
- * implicitly declared methods synthesized by the compiler, can be
- * found in section 8.9 of
- * <cite>The Java&trade; Language Specification</cite>.
+ * 关于枚举的更多信息，包括由编译器合成的隐式声明方法的描述，
+ * 可以在<cite>The Java&trade; Language Specification</cite>
+ * 的8.9章节找到。
  *
- * <p> Note that when using an enumeration type as the type of a set
- * or as the type of the keys in a map, specialized and efficient
- * {@linkplain java.util.EnumSet set} and {@linkplain
- * java.util.EnumMap map} implementations are available.
+ * <p>注意当使用枚举类型作为一个集合的类型或者作为map中键的类型时，
+ * 现成类{@linkplain java.util.EnumSet set} 和 {@linkplain
+ * java.util.EnumMap map}的实现都是专业和高效的。
  *
- * @param <E> The enum type subclass
+ * @param <E> 枚举类型子类
  * @author  Josh Bloch
  * @author  Neal Gafter
  * @see     Class#getEnumConstants()
@@ -54,65 +52,56 @@ import java.io.ObjectStreamException;
  */
 public abstract class Enum<E extends Enum<E>>
         implements Comparable<E>, Serializable {
+
     /**
-     * The name of this enum constant, as declared in the enum declaration.
-     * Most programmers should use the {@link #toString} method rather than
-     * accessing this field.
+     * 枚举常量的名称，在枚举声明中进行声明。
+     * 大多数程序员应使用{@link #toString}方法而不是访问该域。
      */
     private final String name;
 
     /**
-     * Returns the name of this enum constant, exactly as declared in its
-     * enum declaration.
+     * 
+     * 返回枚举常量的名称，在其枚举声明中进行声明。
      *
-     * <b>Most programmers should use the {@link #toString} method in
-     * preference to this one, as the toString method may return
-     * a more user-friendly name.</b>  This method is designed primarily for
-     * use in specialized situations where correctness depends on getting the
-     * exact name, which will not vary from release to release.
+     * <b>大多数程序员应使用{@link #toString}方法而不是该方法，因为toString方法
+     * 能返回对用户更友好的名称 </b>此方法主要设计用于特殊情形，其正确性依赖于获取到的确切名称，
+     * 其名称不会随版本的改变而改变。
      *
-     * @return the name of this enum constant
+     * @return 枚举常量的名称
      */
     public final String name() {
         return name;
     }
 
     /**
-     * The ordinal of this enumeration constant (its position
-     * in the enum declaration, where the initial constant is assigned
-     * an ordinal of zero).
+     * 枚举常量的序数（其在枚举声明中的位置，首个常量的序数指定为0）。
      *
-     * Most programmers will have no use for this field.  It is designed
-     * for use by sophisticated enum-based data structures, such as
-     * {@link java.util.EnumSet} and {@link java.util.EnumMap}.
+     * 大多数程序要不需要使用此域。
+     * 其被设计用于基于枚举的负责数据结构，
+     * 比如{@link java.util.EnumSet} 和 {@link java.util.EnumMap}。
+
      */
     private final int ordinal;
 
     /**
-     * Returns the ordinal of this enumeration constant (its position
-     * in its enum declaration, where the initial constant is assigned
-     * an ordinal of zero).
+     * 返回枚举常量的序数（其在枚举声明中的位置，首个常量的序数指定为0）。
      *
-     * Most programmers will have no use for this method.  It is
-     * designed for use by sophisticated enum-based data structures, such
-     * as {@link java.util.EnumSet} and {@link java.util.EnumMap}.
+     * 大多数程序要不需要使用此方法。
+     * 其被设计用于基于枚举的负责数据结构，
+     * 比如{@link java.util.EnumSet} 和 {@link java.util.EnumMap}。
      *
-     * @return the ordinal of this enumeration constant
+     * @return 枚举常量的序数
      */
     public final int ordinal() {
         return ordinal;
     }
 
     /**
-     * Sole constructor.  Programmers cannot invoke this constructor.
-     * It is for use by code emitted by the compiler in response to
-     * enum type declarations.
+     * 单独的类构造方法。程序员无法调用该构造方法。
+     * 用于编译器代码生成，以响应枚举类型声明。
      *
-     * @param name - The name of this enum constant, which is the identifier
-     *               used to declare it.
-     * @param ordinal - The ordinal of this enumeration constant (its position
-     *         in the enum declaration, where the initial constant is assigned
-     *         an ordinal of zero).
+     * @param name - 枚举常量的名称，该名称是用于声明该常量的标识符。
+     * @param ordinal - 枚举常量的序数 （枚举常量中所处的位置，首个常量的序数是0）。
      */
     protected Enum(String name, int ordinal) {
         this.name = name;
@@ -120,57 +109,51 @@ public abstract class Enum<E extends Enum<E>>
     }
 
     /**
-     * Returns the name of this enum constant, as contained in the
-     * declaration.  This method may be overridden, though it typically
-     * isn't necessary or desirable.  An enum type should override this
-     * method when a more "programmer-friendly" string form exists.
+     * 返回枚举常量的名称，它（已）包含在了声明中。
+     * 该方法可能被重写，尽管通常不需要这么做。
+     * 当出现一个更为“程序员友好”的字符串格式，应该重写这个方法。
      *
-     * @return the name of this enum constant
+     * @return 枚举类型的名称
      */
     public String toString() {
         return name;
     }
 
     /**
-     * Returns true if the specified object is equal to this
-     * enum constant.
+     * 指定的对象等于枚举常量则返回真。
      *
-     * @param other the object to be compared for equality with this object.
-     * @return  true if the specified object is equal to this
-     *          enum constant.
+     * @param other 与之进行等同性比较的对象。
+     * @return  指定的对象等于枚举常量则返回真。
      */
     public final boolean equals(Object other) {
         return this==other;
     }
 
     /**
-     * Returns a hash code for this enum constant.
+     * 为枚举常量返回哈希码
      *
-     * @return a hash code for this enum constant.
+     * @return 枚举常量的哈希码
      */
     public final int hashCode() {
         return super.hashCode();
     }
 
     /**
-     * Throws CloneNotSupportedException.  This guarantees that enums
-     * are never cloned, which is necessary to preserve their "singleton"
-     * status.
+     * 抛出 CloneNotSupportedException。
+     * 以确保枚举对象们不会被克隆，这对他们保持“单例”状态是必要的。
      *
-     * @return (never returns)
+     * @return (永不返回)
      */
     protected final Object clone() throws CloneNotSupportedException {
         throw new CloneNotSupportedException();
     }
 
     /**
-     * Compares this enum with the specified object for order.  Returns a
-     * negative integer, zero, or a positive integer as this object is less
-     * than, equal to, or greater than the specified object.
+     * 比较该对象与指定对象的序数。
+     * 从序数上，根据该对象小于、等于或者大于指定对象，分别返回相应的负数，0，或者正数。
      *
-     * Enum constants are only comparable to other enum constants of the
-     * same enum type.  The natural order implemented by this
-     * method is the order in which the constants are declared.
+     * 枚举常量只与相同枚举类型的常量进行比较。
+     * 该方法实现的自然顺序就是常量声明时的顺序。
      */
     public final int compareTo(E o) {
         Enum<?> other = (Enum<?>)o;
@@ -182,16 +165,14 @@ public abstract class Enum<E extends Enum<E>>
     }
 
     /**
-     * Returns the Class object corresponding to this enum constant's
-     * enum type.  Two enum constants e1 and  e2 are of the
-     * same enum type if and only if
-     *   e1.getDeclaringClass() == e2.getDeclaringClass().
-     * (The value returned by this method may differ from the one returned
-     * by the {@link Object#getClass} method for enum constants with
-     * constant-specific class bodies.)
+     * 返回与枚举常量对应的枚举类型的Class对象。
+     * 当且仅当e1和e2满足条件：e1.getDeclaringClass() == e2.getDeclaringClass()，
+     * 枚举常量e1和e2的枚举类型才相同。
+	 * （该方法返回的值不同于{@link Object#getClass}方法返回值，
+	 * {@link Object#getClass}方法用于带有特定常量的类主题的枚举常量）
      *
-     * @return the Class object corresponding to this enum constant's
-     *     enum type
+     * @return 与枚举常量对应的枚举类型的Class对象。
+     *     
      */
     @SuppressWarnings("unchecked")
     public final Class<E> getDeclaringClass() {
@@ -201,30 +182,22 @@ public abstract class Enum<E extends Enum<E>>
     }
 
     /**
-     * Returns the enum constant of the specified enum type with the
-     * specified name.  The name must match exactly an identifier used
-     * to declare an enum constant in this type.  (Extraneous whitespace
-     * characters are not permitted.)
+     * 返回指定枚举类型、指定名称的枚举常量。
+     * 名称必须与在此类型中声明枚举常量所用的标识符完全匹配。（不允许使用额外的空白字符。）
      *
-     * <p>Note that for a particular enum type {@code T}, the
-     * implicitly declared {@code public static T valueOf(String)}
-     * method on that enum may be used instead of this method to map
-     * from a name to the corresponding enum constant.  All the
-     * constants of an enum type can be obtained by calling the
-     * implicit {@code public static T[] values()} method of that
-     * type.
+     * <p>注意对于确切的枚举类型{@code T}，隐式声明的{@code public static T valueOf(String)}
+     * 方法常被使用来映射名称到对一个枚举常量的关系，而不是使用该方法。
+     * 要获取所有的枚举类型常量，可以通过调用隐式的{@code public static T[] values()}
+     * 方法来实现。
      *
-     * @param <T> The enum type whose constant is to be returned
-     * @param enumType the {@code Class} object of the enum type from which
-     *      to return a constant
-     * @param name the name of the constant to return
-     * @return the enum constant of the specified enum type with the
-     *      specified name
-     * @throws IllegalArgumentException if the specified enum type has
-     *         no constant with the specified name, or the specified
-     *         class object does not represent an enum type
-     * @throws NullPointerException if {@code enumType} or {@code name}
-     *         is null
+     * @param <T> 要从中返回常量的枚举类型的 Class对象
+     * @param enumType 枚举类型的{@code Class}对象，据此返回一个常量
+     * @param name 待返回的常量名
+     * @return 具有指定枚举类型、指定名称的枚举常量
+     * @throws IllegalArgumentException 若指定的枚举类型不具有指定名称的常量，
+     * 		        或者指定的类对象不代表枚举类型。
+     * @throws NullPointerException 若{@code enumType} 或 {@code name}
+     *         为null
      * @since 1.5
      */
     public static <T extends Enum<T>> T valueOf(Class<T> enumType,
@@ -239,12 +212,12 @@ public abstract class Enum<E extends Enum<E>>
     }
 
     /**
-     * enum classes cannot have finalize methods.
+     * enum类不能拥有finalize方法。
      */
     protected final void finalize() { }
 
     /**
-     * prevent default deserialization
+     * 阻止缺省的反序列化
      */
     private void readObject(ObjectInputStream in) throws IOException,
         ClassNotFoundException {
